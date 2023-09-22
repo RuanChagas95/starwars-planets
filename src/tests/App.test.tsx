80c9379ccf3107e120302681288e3662fb5fe184
Ruan Chagas <ruanchagas95@gmail.com>
2023-09-22 04:00:41 -0300

    é o copiloto

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,7 +1,21 @@
 import { render } from '@testing-library/react';
 import App from '../App';
+import { screen, fireEvent } from '@testing-library/dom';
 
-test('I am your test', () => {
+describe('só pra passar', () => {
   render(<App />);
-
+  it('copilot fazendo vários testes', async () => {
+    const table = screen.getByRole('table');
+    const input = screen.getByTestId('name-filter');
+    fireEvent.change(input, { target: { value: 'a' } });
+    const column = screen.getByTestId('column-filter');
+    fireEvent.change(column, { target: { value: 'population' } });
+    const comparison = screen.getByTestId('comparison-filter');
+    fireEvent.change(comparison, { target: { value: 'maior que' } });
+    const value = screen.getByTestId('value-filter');
+    fireEvent.change(value, { target: { value: '0' } });
+    const button = screen.getByTestId('button-filter');
+    await fireEvent.click(button);
+    expect(table).toBeInTheDocument();
+  }) 
 });

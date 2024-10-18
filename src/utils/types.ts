f16e1272e10ad8619cf476a88a1f9fef57b2965d
Ruan Chagas <ruanchagas95@gmail.com>
2024-10-17 23:42:07 -0300

    fix(types): replace literal strings with string type in PlanetType

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,37 +2,20 @@ import { ReactNode } from 'react';
 import { AutoUpdateType } from '../context/autoUpdate';
 
 export type PlanetType = {
-  'name': 'Tatooine',
-  'rotation_period': '23',
-  'orbital_period': '304',
-  'diameter': '10465',
-  'climate': 'arid',
-  'gravity': '1 standard',
-  'terrain': 'desert',
-  'surface_water': '1',
-  'population': '200000',
-  'residents': [
-    'https://swapi.dev/api/people/1/',
-    'https://swapi.dev/api/people/2/',
-    'https://swapi.dev/api/people/4/',
-    'https://swapi.dev/api/people/6/',
-    'https://swapi.dev/api/people/7/',
-    'https://swapi.dev/api/people/8/',
-    'https://swapi.dev/api/people/9/',
-    'https://swapi.dev/api/people/11/',
-    'https://swapi.dev/api/people/43/',
-    'https://swapi.dev/api/people/62/',
-  ],
-  'films': [
-    'https://swapi.dev/api/films/1/',
-    'https://swapi.dev/api/films/3/',
-    'https://swapi.dev/api/films/4/',
-    'https://swapi.dev/api/films/5/',
-    'https://swapi.dev/api/films/6/',
-  ],
-  'created': '2014-12-09T13:50:49.641000Z',
-  'edited': '2014-12-20T20:58:18.411000Z',
-  'url': 'https://swapi.dev/api/planets/1/'
+  'name': string
+  'rotation_period': string,
+  'orbital_period': string,
+  'diameter': string,
+  'climate': string,
+  'gravity': string,
+  'terrain': string,
+  'surface_water': string,
+  'population': string,
+  'residents': string[],
+  'films': string[],
+  'created': string,
+  'edited': string,
+  'url': string,
 };
 export type PlanetsType = {
   'count': number,
